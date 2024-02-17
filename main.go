255fd8dbe08e9088a7d49c68b474f03bd1fd5b73
krostyle <diegoo.albarran@gmail.com>
2024-02-17 04:14:21 -0300

    add port

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	setup "github.com/krostyle/auth-systme-go/src/infrastructure/config"
 )
@@ -8,7 +10,8 @@ import (
 func main() {
 	app := fiber.New()
 	setup.Setup(app)
-	if err := app.Listen(":3000"); err != nil {
+	port := os.Getenv("APP_PORT")
+	if err := app.Listen(port); err != nil {
 		panic(err)
 	}
 }
