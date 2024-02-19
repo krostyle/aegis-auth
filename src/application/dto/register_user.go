a99affaea5e49fb4b94e8205a50da71515c6a4c1
krostyle <diegoo.albarran@gmail.com>
2024-02-19 14:32:26 -0300

    add register user DTO

--- src/application/dto/register_user.go
+++ src/application/dto/register_user.go
@@ -1,4 +1,8 @@
 package dto
 
 type RegisterUserDTO struct {
+	Name     string `json:"name"`
+	Lastname string `json:"lastname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
