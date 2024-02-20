062424f93ffbd6d5bd9c02326dc9397812bc0fa8
krostyle <diegoo.albarran@gmail.com>
2024-02-20 09:20:28 -0300

    add CORS

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/krostyle/auth-systme-go/src/infrastructure/config"
+	"github.com/krostyle/auth-systme-go/src/infrastructure/middleware"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(middleware.CORS())
 	config.Setup(app)
 	port := os.Getenv("PORT")
 	env := os.Getenv("ENV")
